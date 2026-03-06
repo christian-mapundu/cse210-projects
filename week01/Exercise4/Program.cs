de8dbeec5b81976edb21f0a8bc4cb466f3fece2e
christian-mapundu <christianmapundu@gmail.com>
2026-03-06 19:39:08 +0200

    Completed the week1/Exercise4 assignment.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        while (!(numbers.Contains(0)))
+        // the loop will continue until the user enters 0
+            {
+                Console.Write("Enter a number of Your choice: ");
+                int userNumbers = int.Parse(Console.ReadLine());
+                numbers.Add(userNumbers);
+            }
+    //int sum = numbers.Sum();
+        int sum = 0;
+        for (int i = 0; i < numbers.Count; i++)
+            {
+                sum = sum + numbers[i];
+            }
+        Console.WriteLine();
+        Console.WriteLine("The sum is " + sum);
+        Console.WriteLine("The average is: " + sum / numbers.Count);
+        int largest = numbers.Max();
+        Console.WriteLine("The largest number is: " +  largest);
+        numbers.Sort();
+        Console.WriteLine();
     }
 }
